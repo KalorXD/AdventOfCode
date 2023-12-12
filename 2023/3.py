1398e5fec83fab59e4b545c8feb17fedbbf4aa5f
Gleb <73389574+KalorXD@users.noreply.github.com>
2023-12-12 01:46:01 +0000

    updated third day solution

--- 2023/3.py
+++ 2023/3.py
@@ -1,10 +1,32 @@
 import re
 
-length = len("48.................501....33.....622..............763.........331.................161.683......................................980..........")
-
 with open("2023/input.txt", "r") as f:
-    lines = ''.join([line.strip() for line in f.readlines()])
+    lines = [line.strip() for line in f.readlines()]
 
-a = re.findall(r'\d+', lines)
+def first_part():
+    sum = 0
+    for i in range(len(lines)):
+        if len(set(re.findall(r'\d+', lines[i]))) != len(re.findall(r'\d+', lines[i])):
+            print("uh oh", i)
+        cur_pos = 0
+        for number in re.findall(r'\d+', lines[i]):
+            row, column = i, lines[i].find(number, cur_pos)
+            cur_pos = column + len(number)
+            isPartNumber = False
+            for x in range(row - 1, row + 2):
+                for y in range(column - 1, column + len(number) + 1):
+                    try:
+                        if lines[x][y] not in ".0123456789":
+                            isPartNumber = True
+                    except:
+                        continue
+            if isPartNumber:
+                sum += int(number)
+            
+    print(sum)
 
-# INCOMPLETE
+def second_part():
+    # not completed
+
+first_part()
+second_part()
